8a118a19891936cbdd45f4fb1d242ad2660f204a
arkar1022 <arkar1022@gmail.com>
2024-06-29 10:24:31 +0800

    Added Hero Section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,8 @@
 import exp from "constants";
 import { Spotlight } from "./ui/spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
     return (
@@ -9,14 +12,21 @@ const Hero = () => {
                 <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple " />
                 <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
             </div>
-            <div className="h-[50rem] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+            <div className="h-[50rem] absolute top-0 left-0 w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center">
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
             </div>
-            <div className="flex justify-center">
-                <div className="max-w-[89vw]">
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                     <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                        Dynamic Weeb Magic with Next.js
+                        Dynamic Web Magic with Next.js
                     </h2>
+                    <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless User Expereinces" />
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                        Hi, I&apos;m Ar Kar, a Next.js Developer & an AI Enthusiast
+                    </p>
+                    <a href="#about">
+                        <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right"/>
+                    </a>
                 </div>
             </div>
         </div>
